cae93dbc2e3883f9fdce53d3116dfd2edafb0d9e
Jefferson Asencios <jefferson30.ao@gmail.com>
2025-06-30 23:08:53 -0500

    Solución nuevo proyecto: CUDA

--- build/backend/bindings/py/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/jefy_/Downloads/AdmisionTech/backend/bindings/py
-# Build directory: C:/Users/jefy_/Downloads/AdmisionTech/build/backend/bindings/py
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("../../_deps/pybind11-build")
